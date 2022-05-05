fb59bf829a179ed7d1b8d70f3453f9cfa0c60019
lse99 <nifer0@naver.com>
2022-05-05 14:08:27 +0900

    feat: PhoneTimeService 기능 추가

--- src/main/java/Wooaham/wooaham_server/service/PhoneTimeService.java
+++ src/main/java/Wooaham/wooaham_server/service/PhoneTimeService.java
@@ -1,6 +1,10 @@
 package Wooaham.wooaham_server.service;
 
+import Wooaham.wooaham_server.domain.BaseException;
+import Wooaham.wooaham_server.domain.type.ErrorCode;
+import Wooaham.wooaham_server.domain.user.Student;
 import Wooaham.wooaham_server.repository.ParentRepository;
+import Wooaham.wooaham_server.repository.PhoneTimeAlarmRepository;
 import Wooaham.wooaham_server.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +16,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class PhoneTimeService {
     private final ParentRepository parentRepository;
     private final StudentRepository studentRepository;
+    private final PhoneTimeAlarmRepository phoneTimeAlarmRepository;
 
+    public void findPhoneUsageTime(){
+
+    }
+
+    public void updatePhoneUsageTime(Long userId){
+        Student student = studentRepository.findByUserId(userId)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+
+    }
+
+    public void addPhomeTimeAlarm(){
+
+    }
+
+    public void updatePhoneTimeAlarm(){
+
+    }
+
+    public void deletePhoneTimeAlarm(Long alarmId){
+
+    }
 
 }
