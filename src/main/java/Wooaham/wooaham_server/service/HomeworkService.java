6f850d0bd279bc32a6489ddf0f68fd498bb9cf96
lse99 <nifer0@naver.com>
2022-05-19 17:59:27 +0900

    fix: Homework 부모 권환 확대

--- src/main/java/Wooaham/wooaham_server/service/HomeworkService.java
+++ src/main/java/Wooaham/wooaham_server/service/HomeworkService.java
@@ -4,11 +4,14 @@ import Wooaham.wooaham_server.domain.BaseException;
 import Wooaham.wooaham_server.domain.Homework;
 import Wooaham.wooaham_server.domain.type.ErrorCode;
 import Wooaham.wooaham_server.domain.type.HomeworkType;
+import Wooaham.wooaham_server.domain.type.UserType;
+import Wooaham.wooaham_server.domain.user.Parent;
 import Wooaham.wooaham_server.domain.user.Student;
 import Wooaham.wooaham_server.dto.UserDto;
 import Wooaham.wooaham_server.dto.request.HomeworkRequest;
 import Wooaham.wooaham_server.dto.response.HomeworkResponse;
 import Wooaham.wooaham_server.repository.HomeworkRepository;
+import Wooaham.wooaham_server.repository.ParentRepository;
 import Wooaham.wooaham_server.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,13 +26,22 @@ import java.util.List;
 public class HomeworkService {
     private final HomeworkRepository homeworkRepository;
     private final StudentRepository studentRepository;
+    private final ParentRepository parentRepository;
     private final JwtService jwtService;
 
     @Transactional(readOnly = true)
     public List<HomeworkResponse> findSchoolHomework(){
         UserDto.UserInfo userInfo = jwtService.getUserInfo();
-        Student user = studentRepository.findByUserId(userInfo.getUserId())
-                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        Student user;
+        if(userInfo.getRole().equals(UserType.STUDENT)){
+            user = studentRepository.findByUserId(userInfo.getUserId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        } else if(userInfo.getRole().equals(UserType.PARENT)){
+            Parent parent = parentRepository.findByUserId(userInfo.getUserId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_PARENT));
+            user = studentRepository.findByUserId(parent.getPrimaryStudentId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        } else throw new BaseException(ErrorCode.INVALID_ROLE_TYPE);
         List<Homework> hwList = homeworkRepository.findByUserAndType(user, HomeworkType.SCHOOL);
         List<HomeworkResponse> responses = new ArrayList<>();
         for (Homework h : hwList)
@@ -40,8 +52,16 @@ public class HomeworkService {
     @Transactional(readOnly = true)
     public List<HomeworkResponse> findAcademyHomework(){
         UserDto.UserInfo userInfo = jwtService.getUserInfo();
-        Student user = studentRepository.findByUserId(userInfo.getUserId())
-                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        Student user;
+        if(userInfo.getRole().equals(UserType.STUDENT)){
+            user = studentRepository.findByUserId(userInfo.getUserId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        } else if(userInfo.getRole().equals(UserType.PARENT)){
+            Parent parent = parentRepository.findByUserId(userInfo.getUserId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_PARENT));
+            user = studentRepository.findByUserId(parent.getPrimaryStudentId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        } else throw new BaseException(ErrorCode.INVALID_ROLE_TYPE);
         List<Homework> hwList = homeworkRepository.findByUserAndType(user, HomeworkType.ACADEMY);
         List<HomeworkResponse> responses = new ArrayList<>();
         for (Homework h : hwList)
@@ -51,8 +71,16 @@ public class HomeworkService {
 
     public Long addHomework(HomeworkRequest req){
         UserDto.UserInfo userInfo = jwtService.getUserInfo();
-        Student user = studentRepository.findByUserId(userInfo.getUserId())
-                .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        Student user;
+        if(userInfo.getRole().equals(UserType.STUDENT)){
+            user = studentRepository.findByUserId(userInfo.getUserId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        } else if(userInfo.getRole().equals(UserType.PARENT)){
+            Parent parent = parentRepository.findByUserId(userInfo.getUserId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_PARENT));
+            user = studentRepository.findByUserId(parent.getPrimaryStudentId())
+                    .orElseThrow(() -> new BaseException(ErrorCode.NOTFOUND_STUDENT));
+        } else throw new BaseException(ErrorCode.INVALID_ROLE_TYPE);
         Homework homework = req.toHomework(user);
         homeworkRepository.save(homework);
         return homework.getId();
