3309dc14c6dd82e0c702fd04007f00442d021d88
lse99 <nifer0@naver.com>
2022-05-25 20:11:02 +0900

    fix: FCMService

--- src/main/java/Wooaham/wooaham_server/service/FCMService.java
+++ src/main/java/Wooaham/wooaham_server/service/FCMService.java
@@ -1,12 +1,55 @@
 package Wooaham.wooaham_server.service;
 
+import Wooaham.wooaham_server.dto.request.FCMMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
+import lombok.RequiredArgsConstructor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.net.http.HttpHeaders;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class FCMService {
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/wooaham-72fde/messages:send";
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException{
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody=RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request=new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(org.springframework.http.HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(org.springframework.http.HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
+        FCMMessage fcmMessage = FCMMessage.builder()
+                .message(FCMMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FCMMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build())
+                        .build())
+                .validate_only(false)
+                .build();
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
     private String getAccessToken() throws IOException{
         String firebaseConfigPath = "firebase/wooaham-firebase-service-key.json";
         GoogleCredentials googleCredentials = GoogleCredentials
