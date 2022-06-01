82b65f4f4beb85377269535f39d64888fce03215
juhee <lsy669@naver.com>
2022-06-01 18:36:07 +0900

    add: web socket configuration

---
+++ src/main/java/Wooaham/wooaham_server/utils/TextHandler.java
@@ -0,0 +1,8 @@
+package Wooaham.wooaham_server.utils;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Service
+public class TextHandler extends TextWebSocketHandler {
+}
